23a30a06cbcb260b1e4071602a843df3dcbaaf8d
I454075 <bart.pelsma@hotmail.com>
2023-01-02 14:08:20 +0100

    Project Added

---
+++ LC_Authentication/Models/RegisterForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LC_Authentication
+{
+    public class RegisterForm
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public virtual string Email { get; set; }
+        [Required]
+        public virtual string Password { get; set; }
+    }
+}
